ec41e3007f5b0bb90cce84f8524bfbeca81b21c5
Vatsal Sharma <vatsalsharma376@gmail.com>
2022-02-01 20:50:16 +0530

    try2

--- server.js
+++ server.js
@@ -42,6 +42,8 @@ app.use("/api/plaid", plaid);
 
 
 const port = process.env.PORT || 5000;
-
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
 
